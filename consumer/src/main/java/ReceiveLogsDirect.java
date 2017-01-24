55fd68fc967335255cca27aedd77ab8caca66870
Alexander Zhuravlev <kvark.lark@gmail.com>
2017-01-24 14:12:08 +0300

    Add "Routing" example

--- consumer/src/main/java/ReceiveLogsDirect.java
+++ consumer/src/main/java/ReceiveLogsDirect.java
@@ -9,9 +9,9 @@ import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class ReceiveLogs {
+public class ReceiveLogsDirect {
 
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "direct_logs";
 
     public static void main(String[] argv)
             throws IOException, InterruptedException, TimeoutException {
@@ -21,13 +21,18 @@ public class ReceiveLogs {
         Connection connection = factory.newConnection();
         final Channel channel = connection.createChannel();
 
-        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
         String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, "");
 
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-        channel.basicQos(1); // Accept only one unack-ed message at a time.
 
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
@@ -35,7 +40,7 @@ public class ReceiveLogs {
                                        byte[] body)
                     throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
+                System.out.println(" [x] Received [" + envelope.getRoutingKey() + "]: '" + message + "'");
             }
         };
         channel.basicConsume(queueName, true, consumer);
